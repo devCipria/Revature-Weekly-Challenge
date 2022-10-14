6aa2a1eb8dd397c741e4bdebf061818c28d509d6
devCipria <devcipriati@gmail.com>
2022-10-14 12:10:24 -0400

    Add Linked List implementation

--- Week-2/Linked-List-Challenge/src/main/java/org/example/Main.java
+++ Week-2/Linked-List-Challenge/src/main/java/org/example/Main.java
@@ -1,23 +1,46 @@
 package org.example;
 
+/**
+ * a) Intersection: Given two (singly) linked lists, determine if the two lists intersect.
+ * Return the intersecting node.
+ * Note that the intersection is defined based on reference, not value.
+ * That is, if the kth node of the first linked list is the exact same node (by reference)
+ * as the jth node of the second linked list, then they are intersecting.
+ */
 public class Main {
     public static void main(String[] args) {
-        /**
-         * a) Intersection: Given two (singly) linked lists, determine if the two lists intersect.
-         * Return the intersecting node.
-         * Note that the intersection is defined based on reference, not value.
-         * That is, if the kth node of the first linked list is the exact same node (by reference)
-         * as the jth node of the second linked list, then they are intersecting.
-         */
-
-
-        /**
-         * b) Loop Detection: Given a circular linked list, implement an algorithm that returns the node at the beginning of the loop.
-         * DEFINITION:
-         * Circular linked list: A (corrupt) linked list in which a node's next pointer points to an earlier node, so as to make a loop in the linked list.
-         * EXAMPLE
-         * Input: A -> B -> C -> D -> E -> C [the same C as earlier]
-         * Output: C
-         */
+        LinkedList ll = new LinkedList();
+        ll.append(25);
+        ll.append(50);
+        ll.append(75);
+        ll.append(100);
     }
 }
+
+class Node {
+    Node next;
+    int data;
+    public Node (int data){
+        this.data = data;
+    }
+}
+
+class LinkedList {
+    Node head;
+
+    void append(int data) {
+        if (head == null) {
+            head = new Node(data);
+            return;
+        }
+
+        Node current = head;
+
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = new Node(data);
+    }
+
+}
+
